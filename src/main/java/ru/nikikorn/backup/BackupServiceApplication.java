cdebf9030bb12b052c587ad6d689c69c7cd300dd
nikikorn <nikikorn@gmail.com>
2022-06-30 23:57:06 +0300

    Add feature B3000-6. Read from file or if file not exist set deafult.

--- src/main/java/ru/nikikorn/backup/BackupServiceApplication.java
+++ src/main/java/ru/nikikorn/backup/BackupServiceApplication.java
@@ -9,7 +9,7 @@ public class BackupServiceApplication {
     private static String targetDirectory; //путь до директории, которую нужно забекапить (по умолчанию: там, где лежит jar)
     private static String backupDirectory; // директория, в которую скидываются данные для backup (по умолчанию: там, где лежит jar + /backup)
     private static boolean includeDirectory;  //флаг, показывающий, что нужно копировать внутренние директории и файлы (по умолчанию: false)
-    private static int backupCicleSize; //- кол-во циклов создания backup директорий (по умолчанию: 3)
+    private static Integer backupCicleSize; //- кол-во циклов создания backup директорий (по умолчанию: 3)
 
 
     public static void main(String[] args) throws IOException {
@@ -23,16 +23,16 @@ public class BackupServiceApplication {
             includeDirectory = false;
             backupCicleSize = 3;
         }
-        
-
-        /*try {
+        //берем данные из файла конфиг.тхт
+        System.out.println(properties.getProperty("backupCicleSize"));
+        try {
             targetDirectory = properties.getProperty("targetDirectory");
             backupDirectory = properties.getProperty("backupDirectory");
             includeDirectory = Boolean.parseBoolean(properties.getProperty("includeDirectory"));
-            backupCicleSize = Integer.parseInt(properties.getProperty("backupCicleSize"));
+            backupCicleSize = Integer.valueOf(properties.getProperty("backupCicleSize").trim());
         } catch (NumberFormatException e) {
-            System.out.println("tyt");
-        }*/
+            e.printStackTrace();
+        }
 
 
     }
