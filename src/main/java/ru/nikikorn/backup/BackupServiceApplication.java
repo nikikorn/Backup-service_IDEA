6b5256aba442ce2c33b58f6bacda197c703626b2
nikikorn <nikikorn@gmail.com>
2022-07-01 10:33:48 +0300

    PullRequest remark correction. New class was added.

--- src/main/java/ru/nikikorn/backup/BackupServiceApplication.java
+++ src/main/java/ru/nikikorn/backup/BackupServiceApplication.java
@@ -6,34 +6,12 @@ import java.io.IOException;
 import java.util.Properties;
 
 public class BackupServiceApplication {
-    private static String targetDirectory; //путь до директории, которую нужно забекапить (по умолчанию: там, где лежит jar)
-    private static String backupDirectory; // директория, в которую скидываются данные для backup (по умолчанию: там, где лежит jar + /backup)
-    private static boolean includeDirectory;  //флаг, показывающий, что нужно копировать внутренние директории и файлы (по умолчанию: false)
-    private static Integer backupCicleSize; //- кол-во циклов создания backup директорий (по умолчанию: 3)
+
 
 
     public static void main(String[] args) throws IOException {
-        Properties properties = new Properties();
-        try (FileInputStream inputStream = new FileInputStream("Config.txt")) {
-            properties.load(inputStream);
-        } catch (FileNotFoundException e) {
-            System.out.println("File not found.");
-            targetDirectory = "/Users/nikikorn/IdeaProjects/Backup-service/";
-            backupDirectory = "/Users/nikikorn/IdeaProjects/Backup-service/backup/";
-            includeDirectory = false;
-            backupCicleSize = 3;
-        }
-        //берем данные из файла конфиг.тхт
-        System.out.println(properties.getProperty("backupCicleSize"));
-        try {
-            targetDirectory = properties.getProperty("targetDirectory");
-            backupDirectory = properties.getProperty("backupDirectory");
-            includeDirectory = Boolean.parseBoolean(properties.getProperty("includeDirectory"));
-            backupCicleSize = Integer.valueOf(properties.getProperty("backupCicleSize").trim());
-        } catch (NumberFormatException e) {
-            e.printStackTrace();
-        }
-
+        BackupSettings backupSettings = new BackupSettings();
+        backupSettings.start();
 
     }
 }
