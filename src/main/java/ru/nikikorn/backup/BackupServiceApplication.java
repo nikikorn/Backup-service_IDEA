e9d79af175db00912cae56e4af36f8582cf93fc0
nikikorn <nikikorn@gmail.com>
2022-07-01 18:42:33 +0300

    Variables now NOT static. Now using directory of .jar storage. try-catch block was changed.

--- src/main/java/ru/nikikorn/backup/BackupServiceApplication.java
+++ src/main/java/ru/nikikorn/backup/BackupServiceApplication.java
@@ -3,15 +3,26 @@ package ru.nikikorn.backup;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDate;
 import java.util.Properties;
 
 public class BackupServiceApplication {
 
-
-
     public static void main(String[] args) throws IOException {
         BackupSettings backupSettings = new BackupSettings();
         backupSettings.start();
+
+        /*BackupCopying backupCopying = new BackupCopying();
+        backupCopying.copy();*/
+
+
+
+
+
+
 
     }
 }
--- src/main/java/ru/nikikorn/backup/BackupSettings.java
+++ src/main/java/ru/nikikorn/backup/BackupSettings.java
@@ -1,70 +1,51 @@
 package ru.nikikorn.backup;
 
+
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Properties;
 
 public class BackupSettings {
-    private static String targetDirectory;
-    private static String backupDirectory;
-    private static boolean includeDirectory;
-    private static Integer backupCicleSize;
 
-    public void start() throws IOException {
+    private String targetDirectory;
+    private String backupDirectory;
+    private boolean includeDirectory;
+    private Integer backupCicleSize;
+
+    public void start() {
         Properties properties = new Properties();
-        try (
-                FileInputStream inputStream = new FileInputStream("Config.txt")) {
+        try (FileInputStream inputStream = new FileInputStream("Config.txt")) {
             properties.load(inputStream);
-        } catch (
-                FileNotFoundException e) {
-            System.out.println("Configuration file was not found - the default settings was applied.");
+            targetDirectory = properties.getProperty("targetDirectory","/Users/nikikorn/IdeaProjects/Backup-service/target/");
+            backupDirectory = properties.getProperty("backupDirectory","/Users/nikikorn/IdeaProjects/Backup-service/target/backup/");
+            includeDirectory = Boolean.parseBoolean(properties.getProperty("includeDirectory","false"));
+            backupCicleSize = Integer.valueOf(properties.getProperty("backupCicleSize","3").trim());
+        } catch (IOException e) {
+            System.out.println("Configuration file was not read - the default settings was applied.");
             targetDirectory = "/Users/nikikorn/IdeaProjects/Backup-service/";
             backupDirectory = "/Users/nikikorn/IdeaProjects/Backup-service/backup/";
             includeDirectory = false;
             backupCicleSize = 3;
-        }
-
-        try {
-            targetDirectory = properties.getProperty("targetDirectory");
-            backupDirectory = properties.getProperty("backupDirectory");
-            includeDirectory = Boolean.parseBoolean(properties.getProperty("includeDirectory"));
-            backupCicleSize = Integer.valueOf(properties.getProperty("backupCicleSize").trim());
         } catch (NumberFormatException e) {
-            System.out.println("Incorrect data format in backupCicleSize. Deafult setting was set.");
+            System.out.println("Incorrect data format in backupCicleSize. Default setting was set.");
             backupCicleSize = 3;
         }
     }
 
-    public static String getTargetDirectory() {
+    public String getTargetDirectory() {
         return targetDirectory;
     }
 
-    public static void setTargetDirectory(String targetDirectory) {
-        BackupSettings.targetDirectory = targetDirectory;
-    }
-
-    public static String getBackupDirectory() {
+    public String getBackupDirectory() {
         return backupDirectory;
     }
 
-    public static void setBackupDirectory(String backupDirectory) {
-        BackupSettings.backupDirectory = backupDirectory;
-    }
-
-    public static boolean isIncludeDirectory() {
+    public boolean getIncludeDirectory() {
         return includeDirectory;
     }
 
-    public static void setIncludeDirectory(boolean includeDirectory) {
-        BackupSettings.includeDirectory = includeDirectory;
-    }
-
-    public static Integer getBackupCicleSize() {
+    public Integer getBackupCicleSize() {
         return backupCicleSize;
-    }
-
-    public static void setBackupCicleSize(Integer backupCicleSize) {
-        BackupSettings.backupCicleSize = backupCicleSize;
     }
 }
