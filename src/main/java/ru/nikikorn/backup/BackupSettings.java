4681b268f05dab826cb868dbbc531c3e5d4060b6
nikikorn <108460122+nikikorn@users.noreply.github.com>
2022-07-03 11:35:28 +0300

    Little misunderstanding :D (#4)

---
+++ src/main/java/ru/nikikorn/backup/BackupSettings.java
@@ -0,0 +1,49 @@
+package ru.nikikorn.backup;
+
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Properties;
+
+public class BackupSettings {
+
+    private String targetDirectory;
+    private String backupDirectory;
+    private boolean includeDirectory;
+    private Integer backupCicleSize;
+
+
+    public BackupSettings() {
+        String jar = Path.of(BackupServiceApplication.class.getProtectionDomain().getCodeSource().getLocation().getPath()).getParent().normalize() + "/";
+        Properties properties = new Properties();
+        try (FileInputStream inputStream = new FileInputStream("Config.txt")) {
+            properties.load(inputStream);
+        } catch (IOException e) {
+            System.out.println("Configuration file was not read - the default settings was applied.");
+        }
+
+        targetDirectory = properties.getProperty("targetDirectory", jar);
+        backupDirectory = properties.getProperty("backupDirectory", jar + "backup/");
+        includeDirectory = Boolean.parseBoolean(properties.getProperty("includeDirectory", "false"));
+        backupCicleSize = Integer.valueOf(properties.getProperty("backupCicleSize", "3").trim());
+    }
+
+    public String getTargetDirectory() {
+        return targetDirectory;
+    }
+
+    public String getBackupDirectory() {
+        return backupDirectory;
+    }
+
+    public boolean getIncludeDirectory() {
+        return includeDirectory;
+    }
+
+    public Integer getBackupCicleSize() {
+        return backupCicleSize;
+    }
+}
