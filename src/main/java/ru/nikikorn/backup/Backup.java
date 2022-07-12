cd611889e4611a605526a41db389af1d4034b9f9
nikikorn <nikikorn@gmail.com>
2022-07-12 17:11:53 +0300

    All remarks corrected
    New copy-methode made

--- src/main/java/ru/nikikorn/backup/Backup.java
+++ src/main/java/ru/nikikorn/backup/Backup.java
@@ -10,78 +10,61 @@ import java.util.*;
 import static java.nio.file.Files.*;
 
 public class Backup {
-    private Backup(BackupSettings bs) {
-    }
 
-    final static String WELCOMETEXT = "You are trying to backup this directory - %s. " +
+
+    private final static String WELCOMETEXT = "You are trying to backup this directory - %s. " +
             System.lineSeparator() + "If you type \"Yes\", copies of files will be saved in - %s.";
 
     public static void start(BackupSettings settings) throws IOException {
         System.out.printf((WELCOMETEXT), settings.getTargetDirectory(), settings.getBackupDirectory());
         Scanner scanner = new Scanner(System.in);
-        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("MM-dd-yyyy, HH-mm");
-        String date = dateTimeFormatter.format(LocalDateTime.now());
 
         if (!(scanner.nextLine().equalsIgnoreCase("yes"))) {
-            System.exit(1);
-        } else {
-            Path source = settings.getTargetDirectory();
-            Path destination = Paths.get(settings.getBackupDirectory() + "/" + date);
-            try {
-                Files.createDirectories(destination);
-            } catch (FileAlreadyExistsException e) {
-                Files.copy(destination, destination, StandardCopyOption.REPLACE_EXISTING);
-                System.out.println("Files were successfully overwrited.");
-            }
+            return;
+        }
+        Path source = settings.getTargetDirectory();
+        Path destination = settings.getBackupDirectory();
 
-            if (settings.isIncludeDirectory()) {
-                File[] listFiles = source.toFile().listFiles();
-                try {
-                    for (File file : listFiles) {
-                        if (file.isHidden()) {
-                            continue;
-                        }
-                        if (file.isFile()) {
-                            Files.copy(file.toPath(), Paths.get(destination + "/" + file.toPath().getFileName()), StandardCopyOption.REPLACE_EXISTING);
-                        } else {
-                            Path newDir = null;
-                            try {
-                                newDir = createDirectory(Paths.get(destination + "/" + file.toPath().getFileName()));
-                            } catch (IOException e) {
-                                try {
-                                    Files.copy(file.toPath(), Paths.get(destination + "/" + file.toPath().getFileName()), StandardCopyOption.REPLACE_EXISTING);
-                                } catch (IOException ex) {
-                                    System.out.println("Files were successfully overwrited.");
-                                }
-                            }
-                            File[] inside = file.listFiles();
-                            if (!file.isHidden()) {
-                                for (File fileInside : inside) {
-                                    try {
-                                        Files.copy(fileInside.toPath(), Paths.get(newDir + "/" + fileInside.toPath().getFileName()));
-                                    } catch (NoSuchFileException e) {
-                                        continue;
-                                    }
-                                }
-                            }
-                        }
-                    }
-                } catch (Exception e) {
-                    System.out.println("Backup copying problem. Inform you system administrator.");
+        Backup.copy(source,destination);
+
+    }
+
+    private static void copy(Path source, Path backupDestination) {
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("MM-dd-yyyy, HH-mm");
+        String date = dateTimeFormatter.format(LocalDateTime.now());
+        try {
+            Files.createDirectories(Path.of(backupDestination + "/" + date));
+        } catch (IOException e) {
+            System.out.println("No such file");;
+        }
+        backupDestination = Path.of(backupDestination + "/" + date);
+        File[] listFiles = source.toFile().listFiles();
+            for (File file : listFiles) {
+                if (file.isHidden()) {
+                    continue;
                 }
-            } else {
-                File[] listFiles = source.toFile().listFiles();
-                for (File file : listFiles) {
-                    if (!file.isHidden()) {
-                        if (file.isFile()) {
-                            Files.copy(file.toPath(), Paths.get(destination + "/" + file.toPath().getFileName()), StandardCopyOption.REPLACE_EXISTING);
-                        }
+                if (file.isFile()) {
+                    try {
+                        Files.copy(file.toPath(), Paths.get(backupDestination + "/" + file.toPath().getFileName()), StandardCopyOption.REPLACE_EXISTING);
+                    } catch (IOException e) {
+                        System.out.println("File was successfully overwritted.");
+                    }
+                } else {
+                    try {
+                        Files.copy(file.toPath(), Paths.get(backupDestination + "/" + file.toPath().getFileName()), StandardCopyOption.REPLACE_EXISTING);
+                    } catch (IOException e) {
+                            System.out.println("Directory was successfully overwrited.");
                     }
                 }
             }
         }
     }
-}
+
+
+
+
+
+
 
 
 
